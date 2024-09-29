e2b2c3926c57d0241f15f49d6a8a4213d7b3028a
gadzik <glina41@o2.pl>
2024-09-29 12:28:16 +0200

    jsec - init

---
+++ src/main/java/com/company/jsec/JsecApplication.java
@@ -0,0 +1,13 @@
+package com.company.jsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/jsec/JsecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.jsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
