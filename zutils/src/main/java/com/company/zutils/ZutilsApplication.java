4e44f75539bdfe9708a29f007cc70d66e8781d4a
gadzik <glina41@o2.pl>
2024-10-27 16:29:40 +0100

    27/10/2024 16:30

---
+++ zutils/src/main/java/com/company/zutils/ZutilsApplication.java
@@ -0,0 +1,13 @@
+package com.company.zutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZutilsApplication.class, args);
+	}
+
+}
